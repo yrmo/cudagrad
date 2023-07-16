92117695fc46b723c7b6ed06dfa4163fa6fc8950
Ryan Moore <moorethreads@hey.com>
2023-07-15 22:36:56 -0400

    add README.py

--- README.py
+++ README.py
@@ -1,3 +1,4 @@
+README = f"""\
 # cudagrad⚡️
 
 A small autograd engine, inspired by PyTorch and micrograd
@@ -5,23 +6,7 @@ A small autograd engine, inspired by PyTorch and micrograd
 ## Example
 
 ```cpp
-// c++ -std=c++11 example.cpp && ./a.out
-#include "cudagrad.hpp"
-int main() {
-  auto a = cg::tensor({2, 2}, {2.0, 3.0, 4.0, 5.0});
-  auto b = cg::tensor({2, 2}, {6.0, 7.0, 8.0, 9.0});
-  auto c = cg::tensor({2, 2}, {10.0, 10.0, 10.0, 10.0});
-  auto d = cg::tensor({2, 2}, {11.0, 11.0, 11.0, 11.0});
-  auto e = (cg::matmul(a, b) + c) * d;
-  auto f = cg::sum(e);
-  f.get()->backward();
-
-  using namespace std; // NOLINT(build/namespaces)
-  for (auto& x : f.get()->data_) { cout << x << endl;} // 2794
-  for (auto& x : f.get()->size_) { cout << x << endl;} // 1
-  for (auto& x : a.get()->grad_) { cout << x << endl; } // 143 187 143 187
-  for (auto& x : b.get()->grad_) { cout << x << endl; } // 66 66 88 88
-}
+{open('example.cpp').read().strip()}
 ```
 
 ```py
@@ -70,3 +55,7 @@ sh test.sh
 ```
 
 Running the tests requires: `cmake`, `make`, `torch` installed (on the version of Python accessed by the `python` command), `git`, and a C++ compiler. Note that these requirements are only for when you need to run the tests, otherwise except the C++ compiler they are not needed.
+"""
+
+if __name__ == '__main__':
+  open('README.md', 'w').write(README)
