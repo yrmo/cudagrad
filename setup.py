fb9d48087adad3c92ea123b23ecacfc6bb4d8504
Ryan Moore <moorethreads@hey.com>
2023-07-26 20:35:16 -0400

    ```py >>> dir(cg) ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', 'woof'] >>> cg.woof <function woof at 0x10127bba0> >>> cg.woof() woofy! ```

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 import toml
 from pybind11.setup_helpers import Pybind11Extension, build_ext
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 def get_version_from_toml():
@@ -49,5 +49,6 @@ setup(
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
     python_requires=">=3.7",
+    packages=find_packages(),
     include_package_data=True,
 )
