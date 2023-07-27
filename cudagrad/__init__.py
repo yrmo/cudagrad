218f97205d5d376b5e235f694ce2250d791bef6e
Ryan Moore <moorethreads@hey.com>
2023-07-26 21:05:43 -0400

    include mlp file in init comment out mlp script error

--- cudagrad/__init__.py
+++ cudagrad/__init__.py
@@ -1,4 +1,5 @@
 from .cudagrad_bindings import *  # type: ignore
+from .mlp import Neuron
 
 
 def woof():
--- cudagrad/mlp.py
+++ cudagrad/mlp.py
@@ -102,97 +102,97 @@ for _ in range(50):
     print(_ + 1, loss.data, [f"{x.data:1.2f}" for x in ypred])
 # %%
 
-import random
+# import random
 
-import cudagrad
-from cudagrad import Tensor, tensor
-
-
-class Module:
-    def zero_grad(self):
-        for p in self.parameters():
-            p.grad = 0
-
-    def parameters(self):
-        return []
+# import cudagrad
+# from cudagrad import Tensor, tensor
 
 
-class Neuron(Module):
-    def __init__(self, nin, nonlin=True):
-        self.w = [tensor([1], [random.uniform(-1, 1)]) for _ in range(nin)]
-        self.b = tensor([1], [0])
-        self.nonlin = nonlin
+# class Module:
+#     def zero_grad(self):
+#         for p in self.parameters():
+#             p.grad = 0
 
-    def __call__(self, x):
-        ans = tensor([1], [0])
-        for elem_x in x:
-            for elem_w in self.w:
-                if type(elem_x) != Tensor:
-                    elem_x = tensor([1], [elem_x])
-                ans = ans + (elem_w * elem_x)
-        ans = ans + self.b
-        return ans
-        # act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
-        # return act.relu() if self.nonlin else act
-
-    def parameters(self):
-        return self.w + [self.b]
-
-    def __repr__(self):
-        return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
+#     def parameters(self):
+#         return []
 
 
-# %%
-print(Neuron(2)([0.5, 0.2]).data)
-print(Neuron(2)([tensor([1], [0.5]), tensor([1], [0.2])]).data)
-# Layer(1, 2)
-# MLP(2, [2, 1])
+# class Neuron(Module):
+#     def __init__(self, nin, nonlin=True):
+#         self.w = [tensor([1], [random.uniform(-1, 1)]) for _ in range(nin)]
+#         self.b = tensor([1], [0])
+#         self.nonlin = nonlin
+
+#     def __call__(self, x):
+#         ans = tensor([1], [0])
+#         for elem_x in x:
+#             for elem_w in self.w:
+#                 if type(elem_x) != Tensor:
+#                     elem_x = tensor([1], [elem_x])
+#                 ans = ans + (elem_w * elem_x)
+#         ans = ans + self.b
+#         return ans
+#         # act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
+#         # return act.relu() if self.nonlin else act
+
+#     def parameters(self):
+#         return self.w + [self.b]
+
+#     def __repr__(self):
+#         return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
 
 
-# %%
-class Layer(Module):
-    def __init__(self, nin, nout, **kwargs):
-        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
-
-    def __call__(self, x):
-        out = [n(x) for n in self.neurons]
-        return out[0] if len(out) == 1 else out
-
-    def parameters(self):
-        return [p for n in self.neurons for p in n.parameters()]
-
-    def __repr__(self):
-        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+# # %%
+# print(Neuron(2)([0.5, 0.2]).data)
+# print(Neuron(2)([tensor([1], [0.5]), tensor([1], [0.2])]).data)
+# # Layer(1, 2)
+# # MLP(2, [2, 1])
 
 
-class MLP(Module):
-    def __init__(self, nin, nouts):
-        sz = [nin] + nouts
-        self.layers = [
-            Layer(sz[i], sz[i + 1], nonlin=i != len(nouts) - 1)
-            for i in range(len(nouts))
-        ]
+# # %%
+# class Layer(Module):
+#     def __init__(self, nin, nout, **kwargs):
+#         self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
 
-    def __call__(self, x):
-        for layer in self.layers:
-            x = layer(x)
-        return x
+#     def __call__(self, x):
+#         out = [n(x) for n in self.neurons]
+#         return out[0] if len(out) == 1 else out
 
-    def parameters(self):
-        return [p for layer in self.layers for p in layer.parameters()]
+#     def parameters(self):
+#         return [p for n in self.neurons for p in n.parameters()]
 
-    def __repr__(self):
-        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
+#     def __repr__(self):
+#         return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
 
 
-# %%
-nn = MLP(3, [4, 4, 1])  # i think this ignores our random seed sadly
-for _ in range(50):
-    ypred = [nn(x) for x in xs]
-    loss = sum((a - b) ** 2 for a, b in zip(ypred, ys))
-    for p in nn.parameters():
-        p.grad = 0.0
-    loss.backward()
-    for p in nn.parameters():
-        p.data += -0.05 * p.grad
-    print(_ + 1, loss.data, [f"{x.data:1.2f}" for x in ypred])
+# class MLP(Module):
+#     def __init__(self, nin, nouts):
+#         sz = [nin] + nouts
+#         self.layers = [
+#             Layer(sz[i], sz[i + 1], nonlin=i != len(nouts) - 1)
+#             for i in range(len(nouts))
+#         ]
+
+#     def __call__(self, x):
+#         for layer in self.layers:
+#             x = layer(x)
+#         return x
+
+#     def parameters(self):
+#         return [p for layer in self.layers for p in layer.parameters()]
+
+#     def __repr__(self):
+#         return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
+
+
+# # %%
+# # nn = MLP(3, [4, 4, 1])  # i think this ignores our random seed sadly
+# # for _ in range(50):
+# #     ypred = [nn(x) for x in xs]
+# #     loss = sum((a - b) ** 2 for a, b in zip(ypred, ys))
+# #     for p in nn.parameters():
+# #         p.grad = 0.0
+# #     loss.backward()
+# #     for p in nn.parameters():
+# #         p.data += -0.05 * p.grad
+# #     print(_ + 1, loss.data, [f"{x.data:1.2f}" for x in ypred])
