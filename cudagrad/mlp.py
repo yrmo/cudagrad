966c39173e56c62e32b65d71b4d4d77038be95a0
Ryan Moore <moorethreads@hey.com>
2023-08-05 19:56:44 -0400

    clean

--- cudagrad/mlp.py
+++ cudagrad/mlp.py
@@ -115,8 +115,6 @@ class Layer(Module):
         return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
 
 
-
-
 class MLP(Module):
     def __init__(self, nin, nouts):
         sz = [nin] + nouts
