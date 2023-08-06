77d349df41c24a2192e8ef24161de17b8730b6b4
Ryan Moore <moorethreads@hey.com>
2023-08-06 12:34:16 -0400

    add torch nn.Module Neuron and state_dict

---
+++ tests/test_mlp.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+
+class SingleNeuron(nn.Module):
+    def __init__(self):
+        super(SingleNeuron, self).__init__()
+        self.weight = nn.Parameter(torch.randn(1))
+        self.bias = nn.Parameter(torch.randn(1))
+    def forward(self, x):
+        out = self.weight * x + self.bias
+        # out = torch.sigmoid(out)
+        return out
+
+neuron = SingleNeuron()
+print(neuron.state_dict())
