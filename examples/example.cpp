9c28fff3c2511b1914ed52c782d5bd4041edebc1
Ryan Moore <moorethreads@hey.com>
2023-08-13 17:49:14 -0400

    example.cpp .get()-> -> ->

--- examples/example.cpp
+++ examples/example.cpp
@@ -5,13 +5,13 @@ int main() {
   auto b = cg::tensor({2, 2}, {6.0, 7.0, 8.0, 9.0});
   auto c = cg::tensor({2, 2}, {10.0, 10.0, 10.0, 10.0});
   auto d = cg::tensor({2, 2}, {11.0, 11.0, 11.0, 11.0});
-  auto e = (a.get()->matmul(b) + c) * d;
-  auto f = e.get()->sum();
-  f.get()->backward();
+  auto e = (a->matmul(b) + c) * d;
+  auto f = e->sum();
+  f->backward();
 
   using namespace std;
-  for (auto& x : f.get()->data_) {cout<<x<<" ";} // 2794
-  for (auto& x : f.get()->size_) {cout<<x<<" ";} // 1
-  for (auto& x : a.get()->grad_) {cout<<x<<" ";} // 143 187 143 187
-  for (auto& x : b.get()->grad_) {cout<<x<<" ";} // 66 66 88 88
+  for (auto& x : f->data_) {cout<<x<<" ";} // 2794
+  for (auto& x : f->size_) {cout<<x<<" ";} // 1
+  for (auto& x : a->grad_) {cout<<x<<" ";} // 143 187 143 187
+  for (auto& x : b->grad_) {cout<<x<<" ";} // 66 66 88 88
 }
