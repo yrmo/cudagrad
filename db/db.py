0132f6f91c419dc68d015038767109e9cf3e9bef
Ryan Moore <moorethreads@hey.com>
2023-08-17 19:52:24 -0400

    performance results in postgres initial attempt:
    ```
    performance=> select * from tests;
    -[ RECORD 1 ]----+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
    id               | 2
    key              | tiny matmul
    setup            | import cudagrad as cg;
    statement        | a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b
    version          | NULL
    loop_nanoseconds | 1.9065583
    created_at       | 2023-08-17 23:50:24.895704
    -[ RECORD 2 ]----+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
    id               | 3
    key              | tiny matmul
    setup            | import torch;
    statement        | a = torch.tensor(((2.0, 3.0), (4.0, 5.0))); b = torch.tensor(((6.0, 7.0), (8.0, 9.0))); c = a @ b
    version          | NULL
    loop_nanoseconds | 5.294575
    created_at       | 2023-08-17 23:50:32.935738
    -[ RECORD 3 ]----+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
    id               | 4
    key              | tiny matmul
    setup            | import cudagrad as cg;
    statement        | a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b; d = c.sum(); d.backward()
    version          | NULL
    loop_nanoseconds | 2.6676219
    created_at       | 2023-08-17 23:50:37.785753
    -[ RECORD 4 ]----+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
    id               | 5
    key              | tiny matmul
    setup            | import torch;
    statement        | a = torch.tensor(((2.0, 3.0), (4.0, 5.0)), requires_grad=True); b = torch.tensor(((6.0, 7.0), (8.0, 9.0)), requires_grad=True); c = a @ b; d = c.sum(); d.backward()
    version          | NULL
    loop_nanoseconds | 23.068941
    created_at       | 2023-08-17 23:51:03.030738
    -[ RECORD 5 ]----+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
    id               | 6
    key              | tiny matmul
    setup            | import numpy as np
    statement        | a = np.array([[2.0, 3.0],[4.0, 5.0]]); b = np.array([[6.0, 7.0], [8.0, 9.0]]); c = a @ b;
    version          | NULL
    loop_nanoseconds | 1.779293
    created_at       | 2023-08-17 23:51:10.566239
    ```

--- db/db.py
+++ db/db.py
@@ -1,18 +1,66 @@
+from getpass import getpass
 from subprocess import run
+from timeit import timeit
 
 import boto3
+import fire
+import psycopg2
+
+# imports for timeit, possibly dynamically (?)
+import torch  # type: ignore
+
+import cudagrad as cg  # type: ignore
+
+cmd1 = ("import cudagrad as cg;", "a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b", "tiny matmul")
+cmd2 = ("import torch;", "a = torch.tensor(((2.0, 3.0), (4.0, 5.0))); b = torch.tensor(((6.0, 7.0), (8.0, 9.0))); c = a @ b", "tiny matmul")
+cmd3 = ("import cudagrad as cg;", "a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b; d = c.sum(); d.backward()", "tiny matmul")
+cmd4 = ("import torch;", "a = torch.tensor(((2.0, 3.0), (4.0, 5.0)), requires_grad=True); b = torch.tensor(((6.0, 7.0), (8.0, 9.0)), requires_grad=True); c = a @ b; d = c.sum(); d.backward()", "tiny matmul")
+cmd5 = ("import numpy as np", "a = np.array([[2.0, 3.0],[4.0, 5.0]]); b = np.array([[6.0, 7.0], [8.0, 9.0]]); c = a @ b;", "tiny matmul")
+TESTS = [cmd1, cmd2, cmd3, cmd4, cmd5]
 
 
-def main():
+class DB:
+  def connect(self):
+    run(f"psql -h {HOSTNAME} -p {PORT} -U {USERNAME} -d {DATABASE} -W", shell=True)
+
+  def insert_test_record(*args):
+      connection = psycopg2.connect(
+          host=HOSTNAME,
+          port=PORT,
+          user=USERNAME,
+          password=getpass(),
+          dbname=DATABASE
+      )
+      cursor = connection.cursor()
+      query = """
+      INSERT INTO tests (key, setup, statement, version, loop_nanoseconds)
+      VALUES (%s, %s, %s, %s, %s);
+      """
+      cursor.execute(query, args[1:])
+      connection.commit()
+      cursor.close()
+      connection.close()
+
+  def run_tests(self) -> None:
+    for test in TESTS:
+      print(test)
+      statement = test[1]
+      setup = test[0]
+      key = test[2]
+      version = "NULL" # TODO
+      print(statement, '---------',setup)
+      loop_nanoseconds = timeit(statement, setup)
+      print('---', loop_nanoseconds)
+      self.insert_test_record(key, setup, statement, version, loop_nanoseconds)
+
+
+if __name__ == '__main__':
   rds_client = boto3.client('rds')
   db_instance = rds_client.describe_db_instances(DBInstanceIdentifier='boto-postgres')['DBInstances'][0]
 
   HOSTNAME = db_instance['Endpoint']['Address']
   PORT = db_instance['Endpoint']['Port']
   USERNAME = "ryan"
-  DATABASE = "postgres"
+  DATABASE = "performance"
 
-  run(f"psql -h {HOSTNAME} -p {PORT} -U {USERNAME} -d {DATABASE} -W", shell=True)
-
-if __name__ == '__main__':
-  main()
+  fire.Fire(DB)
--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,8 @@
 create database performance;
 
 create table tests(
-  id serial,
+  id serial PRIMARY KEY,
+  key varchar(255),
   setup varchar(255),
   statement varchar(1000),
   version varchar(255),
@@ -9,9 +10,10 @@ create table tests(
   created_at timestamp DEFAULT current_timestamp
 );
 
-insert into tests(setup, statement, version, loop_nanoseconds) values(
-  'import cudagrad as cg',
-  'a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b',
-  '2.0.0',
-  '1.77'
+insert into tests(key, setup, statement, version, loop_nanoseconds) values(
+  'tiny matmul'
+  ,'import cudagrad as cg'
+  ,'a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b'
+  ,'2.0.0'
+  ,'1.77'
 );
