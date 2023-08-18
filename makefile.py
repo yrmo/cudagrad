3fd2a9e84ed76040ccee348e2cd2b790b93aefaf
Ryan Moore <moorethreads@hey.com>
2023-08-17 22:56:07 -0400

    move db.py to makefile.py:
    ```
    (main) Ryans-MacBook-Air:cudagrad ryan$ python makefile.py DB run_tests
    ('import cudagrad as cg;', 'a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b', 'tiny matmul')
    ('import torch;', 'a = torch.tensor(((2.0, 3.0), (4.0, 5.0))); b = torch.tensor(((6.0, 7.0), (8.0, 9.0))); c = a @ b', 'tiny matmul')
    ('import cudagrad as cg;', 'a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b; d = c.sum(); d.backward()', 'tiny backward')
    ('import torch;', 'a = torch.tensor(((2.0, 3.0), (4.0, 5.0)), requires_grad=True); b = torch.tensor(((6.0, 7.0), (8.0, 9.0)), requires_grad=True); c = a @ b; d = c.sum(); d.backward()', 'tiny backward')
    ('import numpy as np', 'a = np.array([[2.0, 3.0],[4.0, 5.0]]); b = np.array([[6.0, 7.0], [8.0, 9.0]]); c = a @ b;', 'tiny matmul')
    (main) Ryans-MacBook-Air:cudagrad ryan$ git status
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   db/db.py
    	modified:   makefile.py
    	modified:   performance.db
    
    no changes added to commit (use "git add" and/or "git commit -a")
    (main) Ryans-MacBook-Air:cudagrad ryan$ sqlite3 performance.db
    SQLite version 3.39.5 2022-10-14 20:58:05
    Enter ".help" for usage hints.
    sqlite> select * from tests;
    |tiny matmul|import cudagrad as cg;|a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b||2.04522137498134|2023-08-18 02:42:14
    |tiny matmul|import torch;|a = torch.tensor(((2.0, 3.0), (4.0, 5.0))); b = torch.tensor(((6.0, 7.0), (8.0, 9.0))); c = a @ b||5.61912970899721|2023-08-18 02:42:20
    |tiny backward|import cudagrad as cg;|a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b; d = c.sum(); d.backward()||2.69164362500305|2023-08-18 02:42:23
    |tiny backward|import torch;|a = torch.tensor(((2.0, 3.0), (4.0, 5.0)), requires_grad=True); b = torch.tensor(((6.0, 7.0), (8.0, 9.0)), requires_grad=True); c = a @ b; d = c.sum(); d.backward()||23.4155353329843|2023-08-18 02:42:46
    |tiny matmul|import numpy as np|a = np.array([[2.0, 3.0],[4.0, 5.0]]); b = np.array([[6.0, 7.0], [8.0, 9.0]]); c = a @ b;||1.7806378750247|2023-08-18 02:42:48
    |tiny matmul|import cudagrad as cg;|a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b||1.83497187501052|2023-08-18 02:54:04
    |tiny matmul|import torch;|a = torch.tensor(((2.0, 3.0), (4.0, 5.0))); b = torch.tensor(((6.0, 7.0), (8.0, 9.0))); c = a @ b||5.1915505840152|2023-08-18 02:54:09
    |tiny backward|import cudagrad as cg;|a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b; d = c.sum(); d.backward()||2.66845258299145|2023-08-18 02:54:11
    |tiny backward|import torch;|a = torch.tensor(((2.0, 3.0), (4.0, 5.0)), requires_grad=True); b = torch.tensor(((6.0, 7.0), (8.0, 9.0)), requires_grad=True); c = a @ b; d = c.sum(); d.backward()||23.4610237500165|2023-08-18 02:54:35
    |tiny matmul|import numpy as np|a = np.array([[2.0, 3.0],[4.0, 5.0]]); b = np.array([[6.0, 7.0], [8.0, 9.0]]); c = a @ b;||1.77820933400653|2023-08-18 02:54:37
    ```

--- makefile.py
+++ makefile.py
@@ -1,15 +1,32 @@
 import os
 import re
 import shutil
+import sqlite3
 import subprocess
 from functools import wraps
+from getpass import getpass
+from subprocess import run
+from timeit import timeit
 
 import fire
 import toml
-import torch
+
+# imports for timeit below
+# ignore'ing because type checkers dont know this
+import torch  # type: ignore
+
+import cudagrad as cg  # type: ignore
 
 CPP_FILES = "./tests/test.cpp ./src/tensor.hpp ./src/ops.cu"
 
+cmd1 = ("import cudagrad as cg;", "a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b", "tiny matmul")
+cmd2 = ("import torch;", "a = torch.tensor(((2.0, 3.0), (4.0, 5.0))); b = torch.tensor(((6.0, 7.0), (8.0, 9.0))); c = a @ b", "tiny matmul")
+cmd3 = ("import cudagrad as cg;", "a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b; d = c.sum(); d.backward()", "tiny backward")
+cmd4 = ("import torch;", "a = torch.tensor(((2.0, 3.0), (4.0, 5.0)), requires_grad=True); b = torch.tensor(((6.0, 7.0), (8.0, 9.0)), requires_grad=True); c = a @ b; d = c.sum(); d.backward()", "tiny backward")
+cmd5 = ("import numpy as np", "a = np.array([[2.0, 3.0],[4.0, 5.0]]); b = np.array([[6.0, 7.0], [8.0, 9.0]]); c = a @ b;", "tiny matmul")
+TESTS = [cmd1, cmd2, cmd3, cmd4, cmd5]
+
+DATABASE = "performance.db"
 
 def echo(function):
     @wraps(function)
@@ -125,6 +142,55 @@ class Makefile:
                     init_contents,
                 )
             )
+    class DB:
+        """
+        create database performance;
+
+        create table tests(
+            id serial PRIMARY KEY,
+            key varchar(255),
+            setup varchar(255),
+            statement varchar(1000),
+            version varchar(255),
+            loop_nanoseconds real,
+            created_at timestamp DEFAULT current_timestamp
+        );
+
+        insert into tests(key, setup, statement, version, loop_nanoseconds) values(
+            'tiny matmul'
+            ,'import cudagrad as cg'
+            ,'a = cg.tensor([2, 2], [2.0, 3.0, 4.0, 5.0]); b = cg.tensor([2, 2], [6.0, 7.0, 8.0, 9.0]); c = a @ b'
+            ,'2.0.0'
+            ,'1.77'
+        );
+        """
+        def get_password(self) -> str:
+            return getpass()
+
+        def connect(self):
+            run(f"sqlite3 {DATABASE}")
+
+        def insert_test_record(self, *args):
+            connection = sqlite3.connect(DATABASE)
+            cursor = connection.cursor()
+            query = """
+            INSERT INTO tests (key, setup, statement, version, loop_nanoseconds)
+            VALUES (?, ?, ?, ?, ?);
+            """
+            cursor.execute(query, args)
+            connection.commit()
+            cursor.close()
+            connection.close()
+
+        def run_tests(self) -> None:
+            for test in TESTS:
+                print(test)
+                statement = test[1]
+                setup = test[0]
+                key = test[2]
+                version = None # TODO
+                loop_nanoseconds = timeit(statement, setup)
+                self.insert_test_record(key, setup, statement, version, loop_nanoseconds)
 
 
 if __name__ == "__main__":
