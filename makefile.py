e2731c3391edb076854a0aced819e1f1b402622e
Ryan Moore <moorethreads@hey.com>
2023-08-17 23:14:45 -0400

    move check requiremnts to makefile

--- makefile.py
+++ makefile.py
@@ -191,7 +191,22 @@ class Makefile:
                 version = None # TODO
                 loop_nanoseconds = timeit(statement, setup)
                 self.insert_test_record(key, setup, statement, version, loop_nanoseconds)
+    class CheckRequirementsBroken:
+        def check_program(self, program) -> None:
+            """Checks if the specified program is installed"""
+            path = shutil.which(program)
+            if path is None:
+                print(f"{program} is not installed.")
+            else:
+                print(f"{program} is installed at {path}.")
 
+        def run(self):
+            # sudo apt install clang-format
+            # python -m pip install cpplint
+            # TODO cmake...
+            programs = ["clang-format", "cpplint", "cmake"]
+            for program in programs:
+                self.check_program(program)
 
 if __name__ == "__main__":
     fire.Fire(Makefile)
