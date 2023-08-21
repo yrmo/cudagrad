02d6f03cbe08152192316201a2f24fab10455346
Ryan Moore <moorethreads@hey.com>
2023-08-21 09:05:23 -0700

    fixes #68 add wip gh release script

---
+++ gh-release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+version=$(python -c "import toml; print(toml.load('pyproject.toml')['project']['version'])")
+git tag -a $version -m "Release version $version"
+git add cudagrad/__init__.py pyproject.toml
+git commit -m $version
+git push origin $version
+echo "Version $version has been tagged and pushed to GitHub."
+echo "Please write your release notes for version $version, then press Ctrl-D:"
+release_notes=$(</dev/stdin)
+gh release create $version -t $version -n "$release_notes"
+echo "GitHub release for version $version has been created."
+
