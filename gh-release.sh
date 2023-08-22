f5afa82ccf93687331ad3bb21e88c5c8c41fe066
Ryan Moore <moorethreads@hey.com>
2023-08-22 01:39:00 -0700

    use vscode for release notes text editor

--- gh-release.sh
+++ gh-release.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 python makefile.py bump patch
 version=$(python -c "import toml; print(toml.load('pyproject.toml')['project']['version'])")
 git tag -a $version -m "Release version $version"
@@ -6,8 +5,12 @@ git add cudagrad/__init__.py pyproject.toml
 git commit -m $version
 git push origin $version
 echo "Version $version has been tagged and pushed to GitHub."
-echo "Please write your release notes for version $version, then press Ctrl-D:"
-release_notes=$(</dev/stdin)
+file_name="release_notes.txt"
+code --wait $file_name
+release_notes=$(cat $file_name)
+echo "Content captured:"
+echo "$release_notes"
 gh release create $version -t $version -n "$release_notes"
 echo "GitHub release for version $version has been created."
 python makefile.py publish
+rm $file_name
