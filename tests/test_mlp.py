2226379017ada1a10e5fee77667a441761fad60a
Ryan Moore <ryanm.inbox@gmail.com>
2023-12-24 16:06:58 -0500

    test_mlp shows grad is never not zero?:
    $ python tests/test_mlp.py
    [[-6.0408, 6.1632],
     [5.3242, -5.0834]]
    [[0, 0],
     [0, 0]]
    [3.1208, 2.5703]
    [0, 0]
    [[-6.0373, -6.1604]]
    [[0, 0]]
    [8.9376]
    [0]
    [[-0.5963, -0.0062],
     [0.1741, -0.1097]]
    [[0, 0],
     [0, 0]]
    [-0.4237, -0.6666]
    [0, 0]
    [[0.1204, 0.2781]]
    [[0, 0]]
    [-0.458]
    [0]
    0
    --------------------------------------------------------------------------------
    0.0
    0.0
    --------------------------------------------------------------------------------
    0.0
    --------------------------------------------------------------------------------
    0.0
    0.0
    0.0
    0.0
    --------------------------------------------------------------------------------
    0.0
    0.0

--- tests/test_mlp.py
+++ tests/test_mlp.py
@@ -104,8 +104,12 @@
 # In [9]:
 
 from cudagrad import MLP, Tensor
+from cudagrad.nn import sgd, mse
 
 model = MLP()
+
+
+# TEST FORWARD MLP
 
 model.w0.data[[0, 0]] = -6.0408
 model.w0.data[[0, 1]] = 6.1632
@@ -129,3 +133,66 @@ assert round(model(Tensor([2, 1], [0, 0])).item(), 3) == round(0.0713, 3)
 assert round(model(Tensor([2, 1], [0, 1])).item(), 3) == round(0.9198, 3)
 assert round(model(Tensor([2, 1], [1, 0])).item(), 3) == round(0.9220, 3)
 assert round(model(Tensor([2, 1], [1, 1])).item(), 3) == round(0.0639, 3)
+
+# TEST BACKWARD MLP
+
+# OrderedDict([('layer1.weight',
+#               tensor([[-0.5963, -0.0062],
+#                       [ 0.1741, -0.1097]])),
+#              ('layer1.bias', tensor([-0.4237, -0.6666])),
+#              ('layer2.weight', tensor([[0.1204, 0.2781]])),
+#              ('layer2.bias', tensor([-0.4580]))])
+
+# After 4 XOR steps (1 sample each):
+
+# layer1.weight tensor([[0.0054, 0.0054],
+#         [0.0140, 0.0140]])
+# layer1.bias tensor([0.0054, 0.0140])
+# layer2.weight tensor([[0.0556, 0.0747]])
+# layer2.bias tensor([0.2105])
+
+from cudagrad.nn import mse, sgd
+
+model = MLP()
+
+model.w0.data[[0, 0]] = -0.5963
+model.w0.data[[0, 1]] = -0.0062
+model.w0.data[[1, 0]] = 0.1741
+model.w0.data[[1, 1]] = -0.1097
+
+model.b0.data[[0, 0]] = -0.4237
+model.b0.data[[1, 0]] = -0.6666
+
+model.w1.data[[0, 0]] = 0.1204
+model.w1.data[[0, 1]] = 0.2781
+
+model.b1.data[[0, 0]] = -0.4580
+
+print(model.w0)
+print(model.b0)
+print(model.w1)
+print(model.b1)
+
+X = [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]]
+y = [[0.0], [1.0], [1.0], [0.0]]
+
+
+from itertools import product
+
+def positions(tensor):
+    indices = [list(range(size)) for size in tensor.size]
+    for index in product(*indices):
+        yield index
+
+
+for i in range(1):
+    print(i)
+    model.zero_grad()
+    loss = mse(Tensor([1], y[i]), Tensor([2, 1], X[i]))
+    loss.backward()
+    sgd(model, lr=0.1)
+
+    for p in model.parameters():
+        print("-" * 80)
+        for position in positions(p):
+            print(p.grad[list(position)].item())
