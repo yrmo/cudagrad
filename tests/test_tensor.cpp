eb4c07ed0c204a2ca9b52fef0819d01877029eb1
Ryan Moore <ryanm.inbox@gmail.com>
2024-01-03 00:55:49 -0500

    Add [       OK ] SigmoidGauntlet.MatmulAddSigmoid1

--- tests/test_tensor.cpp
+++ tests/test_tensor.cpp
@@ -606,6 +606,59 @@ TEST(SigmoidGauntlet, MatmulAddSigmoid0) {
   EXPECT_NEAR(b0.get()->grad_[1], 0.2350, 0.01);
 }
 
+
+TEST(SigmoidGauntlet, MatmulAddSigmoid1) {
+  // Tensor.sigmoid(w0 @ x + b0)
+
+  // >>> import torch
+  // >>> w0 = torch.tensor([[0.5, 0.5], [0.5, 0.5]], requires_grad=True)
+  // >>> x = torch.tensor([[1.0], [1.0]], requires_grad=True)
+  // >>> b0 = torch.tensor([[0.5], [0.5]], requires_grad=True)
+  // >>> s = torch.sigmoid(w0 @ x + b0)
+  // >>> s
+  // tensor([[0.8176],
+  //         [0.8176]], grad_fn=<SigmoidBackward0>)
+  // >>> l = s.sum()
+  // >>> l
+  // tensor(1.6351, grad_fn=<SumBackward0>)
+  // >>> l.backward()
+  // >>> w0.grad
+  // tensor([[0.1491, 0.1491],
+  //         [0.1491, 0.1491]])
+  // >>> x.grad
+  // tensor([[0.1491],
+  //         [0.1491]])
+  // >>> b0.grad
+  // tensor([[0.1491],
+  //         [0.1491]])
+
+  auto w0 = cg::tensor({2, 2}, {0.5, 0.5, 0.5, 0.5});
+  auto x = cg::tensor({2, 1}, {1.0, 1.0});
+  auto b0 = cg::tensor({2, 1}, {0.5, 0.5});
+  auto muldot = w0.get()->matmul(x) + b0;
+  auto s = muldot.get()->sigmoid();
+  auto l = s.get()->sum();
+  l.get()->backward();
+
+  EXPECT_EQ(w0.get()->grad_.size(), 4);
+
+  EXPECT_NEAR(l.get()->data_[0], 1.6351, 0.01);
+
+  EXPECT_NEAR(s.get()->data_[0], 0.8176, 0.01);
+  EXPECT_NEAR(s.get()->data_[1], 0.8176, 0.01);
+
+  EXPECT_NEAR(w0.get()->grad_[0], 0.1491, 0.01);
+  EXPECT_NEAR(w0.get()->grad_[1], 0.1491, 0.01);
+  EXPECT_NEAR(w0.get()->grad_[2], 0.1491, 0.01);
+  EXPECT_NEAR(w0.get()->grad_[3], 0.1491, 0.01);
+
+  EXPECT_NEAR(x.get()->grad_[0], 0.1491, 0.01);
+  EXPECT_NEAR(x.get()->grad_[1], 0.1491, 0.01);
+
+  EXPECT_NEAR(b0.get()->grad_[0], 0.1491, 0.01);
+  EXPECT_NEAR(b0.get()->grad_[1], 0.1491, 0.01);
+}
+
 TEST(MLP, InnerMatmul) {
   // w0 @ x
 
