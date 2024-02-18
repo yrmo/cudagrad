443242b2a4fbc8e098c820771297c319a3dde2f3
Ryan Moore <ryanm.inbox@gmail.com>
2024-02-17 22:25:21 -0500

    Copy `tensor.so` from lib to cudagrad folder:
    ```
    ryan@Lambda-TensorBook:~/cudagrad$ pip install .
    Defaulting to user installation because normal site-packages is not writeable
    Processing /home/ryan/cudagrad
      Installing build dependencies ... done
      Getting requirements to build wheel ... done
      Preparing metadata (pyproject.toml) ... done
    Building wheels for collected packages: cudagrad
      Building wheel for cudagrad (pyproject.toml) ... done
      Created wheel for cudagrad: filename=cudagrad-0.1.0-cp310-cp310-linux_x86_64.whl size=1460062 sha256=a9da539885df2e580218a83c12fe01d25030e6e9681fc24a1ce59079bf3821ac
      Stored in directory: /tmp/pip-ephem-wheel-cache-pzk9tev8/wheels/6e/97/c6/cc52a8580426cb31cc0243410052955dcf33c64ff395da9f71
    Successfully built cudagrad
    Installing collected packages: cudagrad
      Attempting uninstall: cudagrad
        Found existing installation: cudagrad 0.1.0
        Uninstalling cudagrad-0.1.0:
          Successfully uninstalled cudagrad-0.1.0
    Successfully installed cudagrad-0.1.0
    ryan@Lambda-TensorBook:~/cudagrad$ cd build/
    ryan@Lambda-TensorBook:~/cudagrad/build$ ls
    bdist.linux-x86_64  lib.linux-x86_64-3.10  temp.linux-x86_64-3.10
    ryan@Lambda-TensorBook:~/cudagrad/build$ cd temp.linux-x86_64-3.10/
    ryan@Lambda-TensorBook:~/cudagrad/build/temp.linux-x86_64-3.10$ ls
    CMakeCache.txt  CMakeFiles  cmake_install.cmake  Makefile  pybind11
    ryan@Lambda-TensorBook:~/cudagrad/build/temp.linux-x86_64-3.10$ cd ..
    ryan@Lambda-TensorBook:~/cudagrad/build$ cd lib.linux-x86_64-3.10/
    ryan@Lambda-TensorBook:~/cudagrad/build/lib.linux-x86_64-3.10$ ls
    cudagrad  tensor.so
    ryan@Lambda-TensorBook:~/cudagrad/build/lib.linux-x86_64-3.10$ cd cudagrad/
    ryan@Lambda-TensorBook:~/cudagrad/build/lib.linux-x86_64-3.10/cudagrad$ ls
    __init__.py  linear.py  mlp.py  moons.py  nn.py  tensor.so
    ```
    ```
    ryan@Lambda-TensorBook:/$ python
    Python 3.10.12 (main, Nov 20 2023, 15:14:05) [GCC 11.4.0] on linux
    Type "help", "copyright", "credits" or "license" for more information.
    >>> import cudagrad
    >>> cudagrad.tensor.hello()
    Hello, GPU!
    >>> cudagrad
    <module 'cudagrad' from '/home/ryan/.local/lib/python3.10/site-packages/cudagrad/__init__.py'>
    >>> exit()
    ryan@Lambda-TensorBook:/$ cd /home/ryan/.local/lib/python3.10/site-packages/cudagrad/
    ryan@Lambda-TensorBook:~/.local/lib/python3.10/site-packages/cudagrad$ ls
    __init__.py  linear.py  mlp.py  moons.py  nn.py  __pycache__  tensor.so
    ```

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ from setuptools.command.build_ext import build_ext
 
 from pybind11.setup_helpers import build_ext
 
-
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
         Extension.__init__(self, name, sources=[])
@@ -18,33 +17,28 @@ class CMakeExtension(Extension):
 class CMakeBuild(build_ext):
     def run(self):
         try:
-            subprocess.check_output(['cmake', '--version'])
+            out = subprocess.check_output(['cmake', '--version'])
         except OSError:
-            raise RuntimeError("CMake must be installed to build tensor!")
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
 
-        for extension in self.extensions:
-            self.build_extension(extension)
+        for ext in self.extensions:
+            self.build_extension(ext)
 
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable]
 
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args
-                              , cwd=self.build_temp)
-        subprocess.check_call(['cmake', '--build', '.'], cwd=self.build_temp)
 
-        # print("Listing all files in build_temp directory:")
-        # for root, dirs, files in os.walk(self.build_temp):
-        #     for file in files:
-        #         print(os.path.join(root, file))
-
-        built_lib = os.path.join(self.build_temp,'lib', 'tensor.so')
-        dest_lib = os.path.join(self.build_lib, 'cudagrad', 'tensor.so')
-        move(built_lib, dest_lib)
-
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--build', '.', '--target', ext.name] + build_args, cwd=self.build_temp)
+        subprocess.check_call(['cp', os.path.join(self.build_lib, 'tensor.so'), os.path.join(extdir, 'cudagrad', 'tensor.so')])
 
 def get_version_from_toml():
     data = toml.load("pyproject.toml")
@@ -65,7 +59,7 @@ setup(
     url="https://github.com/cudagrad/cudagrad",
     description="A tensor-valued autograd engine for Python",
     long_description="",
-    ext_modules=[CMakeExtension('cudagrad.tensor')],
+    ext_modules=[CMakeExtension('tensor')],
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
     python_requires=">=3.7",
