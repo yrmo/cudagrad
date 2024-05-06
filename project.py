9d24bbae59ed8b4d8a62bdc8f55cbf7f3a531cf6
Ryan Moore <ryanm.inbox@gmail.com>
2024-05-06 00:37:47 -0400

    WIP move github release script to project file

--- project.py
+++ project.py
@@ -209,6 +209,29 @@ class Project:
         environ["PROFILING"] = "1"
         RUN(f"python -m cProfile -o ./profiles/{model}.prof -m cudagrad.{model}")
 
+    def release(self):
+        self.bump("patch")
+        # TODO
+        """\
+        version=$(python -c "import toml; print(toml.load('pyproject.toml')['project']['version'])")
+        git tag -a $version -m "Release version $version"
+        git add cudagrad/__init__.py pyproject.toml
+        git commit -m $version
+        git push origin $version
+        echo "Version $version has been tagged and pushed to GitHub."
+        file_name="release_notes.txt"
+        code --wait $file_name
+        release_notes=$(cat $file_name)
+        echo "Content captured:"
+        echo "$release_notes"
+        gh release create $version -t $version -n "$release_notes"
+        echo "GitHub release for version $version has been created."
+        """
+        self.publish()
+        """
+        rm $file_name
+        """
+
     class CheckRequirementsBroken:
         def check_program(self, program) -> None:
             """Checks if the specified program is installed"""
