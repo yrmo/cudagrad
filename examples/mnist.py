bd6619858e6558ba305a20518b4aef20f8f4b684
Ryan Moore <ryanm.inbox@gmail.com>
2024-05-09 18:42:17 -0400

    Use native Python for MNIST accuracy

--- examples/mnist.py
+++ examples/mnist.py
@@ -3,6 +3,7 @@ from os.path import isfile
 
 import matplotlib.pyplot as plt
 import numpy as np
+
 from cudagrad import Tensor
 
 filename = "mnist.npz"
@@ -29,15 +30,19 @@ class ZeroNet:
 
 model = ZeroNet()
 
+
 def accuracy() -> int:
     results = []
     for i, test_image in enumerate(test_images):
         output = model(train_images[i].shape)
         output = int(output.data[0, 0].item())
         results.append(output)
+    return (
+        sum(1 for x, y in zip(results, test_labels.flatten().tolist()) if x == y)
+        / len(test_labels)
+        * 100
+    )
 
-    import numpy as np
-    return (np.sum(np.array(results) == test_labels.flatten()) / len(test_labels)) * 100
 
 for i, train_image in enumerate(train_images):
     if i % (len(train_images) // 10) == 0:
@@ -48,8 +53,8 @@ num_row = 3
 num_col = 4
 fig, axes = plt.subplots(num_row, num_col)
 for i in range(num_row * num_col):
-    ax = axes[i//num_col, i%num_col]
-    ax.imshow(test_images[i], cmap='viridis')
+    ax = axes[i // num_col, i % num_col]
+    ax.imshow(test_images[i], cmap="viridis")
     output = model(train_images[i].shape)
     output = int(output.data[0, 0].item())
     ax.set_title(f"Output: {output}")
