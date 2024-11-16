ddd85e49e56ed4650122cc350d468ebbbc1e5f8a
Ryan Moore <ryanm.inbox@gmail.com>
2024-11-16 15:08:54 -0500

    Add PROFILING logic to cudagrad mnist

--- benchmarks/_cudagrad/mnist.py
+++ benchmarks/_cudagrad/mnist.py
@@ -1,12 +1,16 @@
 import urllib.request
-from os.path import isfile
+from os.path import getenv, isfile
 from random import random
 
-import matplotlib.pyplot as plt
 import numpy as np
 from numpy.typing import NDArray
 
 from cudagrad import Module, Tensor
+
+PROFILING = int(getenv("PROFILING", "0"))
+
+if not PROFILING:
+    import matplotlib.pyplot as plt
 
 filename = "mnist.npz"
 if not isfile(filename):
@@ -57,18 +61,19 @@ for i, train_image in enumerate(train_images):
         a = accuracy()
         print(f"{a}%", " " if a < 98 else "🔥")
 
-num_row = 3
-num_col = 4
-fig, axes = plt.subplots(num_row, num_col)
-for i in range(num_row * num_col):
-    ax = axes[i // num_col, i % num_col]
-    ax.imshow(test_images[i], cmap="viridis")
-    output = int(model(train_images[i]).item())
-    ax.set_title(f"Output: {output}")
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
+if not PROFILING:
+    num_row = 3
+    num_col = 4
+    fig, axes = plt.subplots(num_row, num_col)
+    for i in range(num_row * num_col):
+        ax = axes[i // num_col, i % num_col]
+        ax.imshow(test_images[i], cmap="viridis")
+        output = int(model(train_images[i]).item())
+        ax.set_title(f"Output: {output}")
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
 
-fig.tight_layout()
-plt.savefig("./benchmarks/_cudagrad/plots/mnist.jpg")
+    fig.tight_layout()
+    plt.savefig("./benchmarks/_cudagrad/plots/mnist.jpg")
