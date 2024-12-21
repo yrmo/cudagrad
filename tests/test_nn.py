f520f56db84da56abca6885a178705e14e61f305
Ryan Moore <ryanm.inbox@gmail.com>
2024-12-21 02:19:20 -0500

    Add passing test_cross_entropy_loss_cross_backward

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -161,47 +161,42 @@ class TestNN(unittest.TestCase):
         self.assertAlmostEqual(t.grad[[0, 8]].item(), 0.0855, places=3)
         self.assertAlmostEqual(t.grad[[0, 9]].item(), 0.0320, places=3)
 
-    @unittest.skip("Skipping softmax first")
     def test_cross_entropy_loss_cross_backward(self):
         """
-        arr.unsqueeze(0)
-        tensor([[-0.1600,  0.4920,  0.9304, -1.5375, -0.0106,  1.3549, -0.9282,  1.3031,
-                -0.5426, -0.9015]])
-        output = torch.tensor(arr.unsqueeze(0), requires_grad=True)
-        <stdin>:1: UserWarning: To copy construct from a tensor, it is recommended to use sourceTensor.clone().detach() or sourceTensor.clone().detach().requires_grad_(True), rather than torch.tensor(sourceTensor).
-        output = torch.tensor(arr.unsqueeze(0).data, requires_grad=True)
-        target = torch.tensor([5], dtype=torch.long)
-        loss = F.cross_entropy(output, target)
-        loss
-        tensor(1.3642, grad_fn=<NllLossBackward0>)
-        loss.backward()
-        output
+        >>> t = torch.tensor([[-0.1600,  0.4920,  0.9304, -1.5375, -0.0106,  1.3549, -0.9282,  1.3031, -0.5426, -0.9015]], requi
+        res_grad=True)
+        >>> t
         tensor([[-0.1600,  0.4920,  0.9304, -1.5375, -0.0106,  1.3549, -0.9282,  1.3031,
                 -0.5426, -0.9015]], requires_grad=True)
-        output.grad
+        >>> torch.nn.functional.cross_entropy
+        <function cross_entropy at 0x000001F0CD8C1C60>
+        >>> torch.nn.functional.cross_entropy(t, torch.tensor([5]))
+        tensor(1.3642, grad_fn=<NllLossBackward0>)
+        >>> l = torch.nn.functional.cross_entropy(t, torch.tensor([5]))
+        >>> l.backward()
+        >>> t
+        tensor([[-0.1600,  0.4920,  0.9304, -1.5375, -0.0106,  1.3549, -0.9282,  1.3031,
+                -0.5426, -0.9015]], requires_grad=True)
+        >>> t.grad
         tensor([[ 0.0562,  0.1078,  0.1672,  0.0142,  0.0652, -0.7444,  0.0261,  0.2427,
                 0.0383,  0.0268]])
-        target
-        tensor([5])
-        target.grad
+        >>> l
+        tensor(1.3642, grad_fn=<NllLossBackward0>)
         """
-        output = cudagrad.Tensor([1, 10], [-0.1600,  0.4920,  0.9304, -1.5375, -0.0106,  1.3549, -0.9282,  1.3031, -0.5426, -0.9015])
-        target = cudagrad.Tensor([1], [5.0])
-        loss = cudagrad.nn.cross_entropy(output, target)
-        self.assertAlmostEqual(loss.item(), 1.364, places=3)
-        loss.backward()
-
-        self.assertAlmostEqual(target.grad[0, 0].item(), 0.0562, places=3)
-        self.assertAlmostEqual(target.grad[0, 1].item(), 0.1078, places=3)
-        self.assertAlmostEqual(target.grad[0, 2].item(), 0.1672, places=3)
-        self.assertAlmostEqual(target.grad[0, 3].item(), 0.0142, places=3)
-        self.assertAlmostEqual(target.grad[0, 4].item(), 0.0652, places=3)
-
-        self.assertAlmostEqual(target.grad[0, 5].item(), -0.7444, places=3)
-        self.assertAlmostEqual(target.grad[0, 6].item(), 0.0261, places=3)
-        self.assertAlmostEqual(target.grad[0, 7].item(), 0.2427, places=3)
-        self.assertAlmostEqual(target.grad[0, 8].item(), 0.0383, places=3)
-        self.assertAlmostEqual(target.grad[0, 9].item(), 0.0268, places=3)
+        t = cudagrad.Tensor([1, 10], [-0.1600,  0.4920,  0.9304, -1.5375, -0.0106,  1.3549, -0.9282,  1.3031, -0.5426, -0.9015])
+        l = cudagrad.nn.cross_entropy(t, [0, 5])
+        l.backward()
+        self.assertAlmostEqual(l.item(), 1.364, places=3)
+        self.assertAlmostEqual(t.grad[[0, 0]].item(), 0.0562, places=3)
+        self.assertAlmostEqual(t.grad[[0, 1]].item(), 0.1078, places=3)
+        self.assertAlmostEqual(t.grad[[0, 2]].item(), 0.1672, places=3)
+        self.assertAlmostEqual(t.grad[[0, 3]].item(), 0.0142, places=3)
+        self.assertAlmostEqual(t.grad[[0, 4]].item(), 0.0652, places=3)
+        self.assertAlmostEqual(t.grad[[0, 5]].item(), -0.7444, places=3)
+        self.assertAlmostEqual(t.grad[[0, 6]].item(), 0.0261, places=3)
+        self.assertAlmostEqual(t.grad[[0, 7]].item(), 0.2427, places=3)
+        self.assertAlmostEqual(t.grad[[0, 8]].item(), 0.0383, places=3)
+        self.assertAlmostEqual(t.grad[[0, 9]].item(), 0.0268, places=3)
 
 if __name__ == "__main__":
     unittest.main()
