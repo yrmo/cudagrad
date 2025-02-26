784b2dbf0d70f41079092dc7a3c37369c6087032
Ryan Moore <ryanm.inbox@gmail.com>
2025-02-26 00:14:15 -0500

    Fix tensor.so destination without Release/Debug build folder

--- setup.py
+++ setup.py
@@ -53,19 +53,30 @@ class CMakeBuild(build_ext):
         )
         source_extension = "dll" if platform.system() == "Windows" else "so"
         target_extension = "pyd" if platform.system() == "Windows" else "so"
+        open("build.log", "w").write(self.build_lib)
         copy(
-            os.path.join(self.build_lib, cfg, f"tensor.{source_extension}"),
+            (
+                os.path.join(self.build_lib, cfg, f"tensor.{source_extension}")
+                if os.path.exists(
+                    os.path.join(self.build_lib, cfg, f"tensor.{source_extension}")
+                )
+                else os.path.join(self.build_lib, f"tensor.{source_extension}")
+            ),
             os.path.join(extdir, "cudagrad", f"tensor.{target_extension}"),
         )
- 
+
         try:
             # for development convenience only so 'import cudagrad' has up to date build
             copy(
                 os.path.join(self.build_lib, cfg, f"tensor.{source_extension}"),
-                os.path.join(str((Path().home() / 'cudagrad' / 'cudagrad').resolve()), f"tensor.{target_extension}"),
+                os.path.join(
+                    str((Path().home() / "cudagrad" / "cudagrad").resolve()),
+                    f"tensor.{target_extension}",
+                ),
             )
         except:
             pass
+
 
 def get_version_from_toml() -> str:
     data = toml.load("pyproject.toml")
